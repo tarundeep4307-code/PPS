fa371effd69596ad8cd659e830fe495c3bec2601
tarundeep4307-code <tarundeep4307@gmail.com>
2025-08-28 10:02:03 +0530

    Add files via upload

---
+++ switch.c.c
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main(){
+ int DAY=2;
+ switch(DAY){
+      case 1: printf("MONDAY");
+             break;
+      case 2: printf("TUESDAY");
+             break;
+      case 3: printf("WEDNESDAY");
+             break;
+      case 4: printf("THURSDAY");
+             break;
+      case 5: printf("FRIDAY");
+             break;
+}
+    return 0;
+}
